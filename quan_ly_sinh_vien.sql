a51b2da202ac31bc7c84e62a2f754a51d979db60
TruongTienToan <truongtientoan2308@gmail.com>
2023-04-25 09:48:02 +0700

    bt md3

--- quan_ly_sinh_vien.sql
+++ quan_ly_sinh_vien.sql
@@ -1,26 +1,21 @@
-use quan_ly_sinh_vien;
+-- Hiển thị danh sách tất cả các học viên
+select * from student;
 
-INSERT INTO Class
-values (1, 'A1', '2008-12-20', 1);
-INSERT INTO Class
-VALUES (2, 'A2', '2008-12-22', 1);
-INSERT INTO Class
-VALUES (3, 'B3', current_date, 0);
+-- Hiển thị danh sách các học viên đang theo học
+select * from student
+where Status = true;
 
-insert into student (StudentId, StudentName, Address, Phone, Status, ClassId)
-values (1, 'Hung', 'Ha Noi', 0912113113, 1, 1);
-insert into student (StudentId, StudentName, Address, Status, ClassId)
-values (2, 'Hoa', 'Hai Phong', 1, 1);
-insert into student (StudentId, StudentName, Address, Phone, Status, ClassId)
-values (3, 'Manh', 'HCM', 0123123123, 0, 2);
+-- Hiển thị danh sách các môn học có thời gian học nhỏ hơn 10 giờ 
+select * from subject
+where credit < 10;
 
-INSERT INTO Subject
-VALUES (1, 'CF', 5, 1),
-       (2, 'C', 6, 1),
-       (3, 'HDJ', 5, 1),
-       (4, 'RDBMS', 10, 1); 
-       
-INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
-VALUES (1, 1, 8, 1),
-       (1, 2, 10, 2),
-       (2, 1, 12, 1);	
+-- Hiển thị danh sach học viên lớp A1
+select st.StudentId, st.StudentName, st.Address, st.Phone
+from student st
+join class cl on st.ClassId = cl.ClassID
+where cl.ClassName = 'A1';
+
+-- Hiển thị điểm môn CF của các học viên
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
