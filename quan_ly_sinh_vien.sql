be6e4be516540160656271b809e2afb1d308e61b
TruongTienToan <truongtientoan2308@gmail.com>
2023-04-25 10:27:45 +0700

    bt md3

--- quan_ly_sinh_vien.sql
+++ quan_ly_sinh_vien.sql
@@ -19,3 +19,30 @@ where cl.ClassName = 'A1';
 SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
 FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
 WHERE Sub.SubName = 'CF';
+
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bằng ký tự 'h'
+select * from student
+where StudentName like 'h%';
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12
+select * from class
+where month(StartDate) = 12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5
+select * from subject
+where credit between 3 and 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+UPDATE Student
+SET ClassID = 2
+WHERE StudentName = 'Hung';
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. 
+-- Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select distinct st.StudentName, sj.SubName, mk.Mark
+from student st
+join mark mk on st.StudentId = mk.StudentId
+join subject sj on mk.SubId = sj.SubId
+order by mk.mark desc, st.StudentName asc ;
+	
